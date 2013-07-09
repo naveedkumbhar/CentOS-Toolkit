d1081084feaaa5e2d7f54347777bf61c8f8e5d57
Mehul Bhatt <mehulsbhatt@hotmail.com>
2013-07-09 08:49:16 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,60 @@
+#!/bin/sh -e
+LICENSE=$( cat << DELIM
+#-----------------------------------------------------------------------------
+# This script is under Testing Status. Please DO NOT USE ON EXISTING SYSTEMS!
+# ************ TESTING ONLY! NO GUARANTEES! ************ 
+# All rights reserved | Copyright (C) 2000-2013 Mehul Bhatt
+# Mehul Bhatt <mehulsbhatt@hotmail.com>
+#-----------------------------------------------------------------------------
+DELIM
+)
+clear;
+# Check if we're good on permissions
+if  [ "$(id -u)" != "0" ]; then
+  echo "ERROR: You must be a root user. Exiting..." 2>&1
+  echo  2>&1
+  exit 1
+fi
+
+# Variable Setting
+DATETIME=$(date +"%Y%m%d%H%M%S")
+ARCH=$(uname -p)
+WWWROOT="/var/www/html"
+HOSTNAME=`hostname`
+SERVER_IP=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{print $1}'`
+WGET="wget -t 3 --no-check-certificate"
+CURL="curl -s"
+LOG_FILE="/tmp/_install-$DATETIME.log"
+
+clear
+echo -e "\n*** This script will install CentOS-Toolkit. Press Enter ***"
+read -n 1 -p "Press any key to continue ... "
+clear
+wget -t 3 --no-check-certificate https://raw.github.com/mehulsbhatt/CentOS-Toolkit/master/functions.sh 
+wget -t 3 --no-check-certificate https://raw.github.com/mehulsbhatt/CentOS-Toolkit/master/functions.sh 
+
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+. "$DIR/functions.sh"
+. "$DIR/common.sh"
+
+get_os_architecture
+get_linux_distribution
+get_ip
+
+clear
+echo -e "\n*** Installing Freeswitch ***"
+read -n 1 -p "Press any key to continue ... "
+clear
+install_freeswitch
+
+clear
+echo -e "\n*** Installing ASTPP ***"
+read -n 1 -p "Press any key to continue ... "
+clear
+install_astpp
+rm -rf /tmp/_INSTALL
+echo ""
+echo "+----------------------------------------------------------------+"
+echo "| Done, enjoy                                                    |"
+echo "+----------------------------------------------------------------+"
